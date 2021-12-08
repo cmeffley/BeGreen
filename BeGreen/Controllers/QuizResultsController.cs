f476a79cf4267e1d5229d258e4fd79f64095ebaa
Chris Meffley <cmeffley@gmail.com>
2021-12-07 20:41:18 -0500

    finished file structures

---
+++ BeGreen/Controllers/QuizResultsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeGreen.Controllers
+{
+    [Route("quizResults")]
+    [ApiController]
+    public class QuizResultsController : ControllerBase
+    {
+    }
+}
