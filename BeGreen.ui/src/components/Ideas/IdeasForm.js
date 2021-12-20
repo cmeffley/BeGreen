e51dcf9c9c771366449fe9c4ab072e2fdc260ad4
Chris Meffley <cmeffley@gmail.com>
2021-12-20 15:06:25 -0500

    started ideas form

--- BeGreen.ui/src/components/Ideas/IdeasForm.js
+++ BeGreen.ui/src/components/Ideas/IdeasForm.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import {
+  Button,
+  Form,
+  Label,
+  Input,
+} from 'reactstrap';
 
 function IdeasForm() {
   return (
     <div>
       <h1>Add A Green Idea</h1>
+      <Form
+        autoComplete='off'
+      >
+        <Label>Add a Green Idea that Others can Do!</Label>
+          <Input
+            name='sharedIdea'
+          />
+        <Label>Add an Image if you want</Label>
+          <Input
+            name='image'
+          />
+        <Button type='submit'>Submit</Button>
+      </Form>
     </div>
   );
 }
