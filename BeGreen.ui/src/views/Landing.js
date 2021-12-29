64c2b5dd3d3c33376c47d7b066b9e3adfe50fb78
Chris Meffley <cmeffley@gmail.com>
2021-12-29 18:04:23 -0500

    added favicon

--- BeGreen.ui/src/views/Landing.js
+++ BeGreen.ui/src/views/Landing.js
@@ -11,21 +11,20 @@ function Landing({ user }) {
   return (
     <>
       <h1 style={{ color: '#fff' }}>It&apos;s Not Easy Being Green</h1>
-      <div className='explain'>
-      <h5>The World is changing, and if we all do a little bit, we can have a massive impact.
-      Take the quiz to see how impactful you are, or
-      sign in and share ideas with others willing to help out.
-      Then take those ideas and add them to your activities, and see what you can create!</h5>
-      </div>
       <br />
       <div>
         <img src={GreenSmall} />
       </div>
       <br />
+      <h5 style={{ color: '#fff' }}>The World is changing, and if we all do a little bit, we can have a massive impact.</h5>
+      <h5 style={{ color: '#fff' }}>Take the quiz to see how impactful you are, or
+      sign in and share ideas with others willing to help out.</h5>
+      <h5 style={{ color: '#fff' }}>Then take those ideas and add them to your activities, and see what grows!</h5>
+      <br />
       <div>
-        { user ? <Button inverted color='red' onClick={signOutUser}>Sign Out</Button>
-          : <Button inverted color='teal' onClick={signInUser}>Sign In</Button>}
-        <Button inverted color='green' onClick={() => history.push('/quiz')}>Take Quiz</Button>
+        { user ? <Button color='red' onClick={signOutUser}>Sign Out</Button>
+          : <Button color='teal' onClick={signInUser}>Sign In</Button>}
+        <Button color='green' onClick={() => history.push('/quiz')}>Take Quiz</Button>
       </div>
     </>
   );
